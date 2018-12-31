94dc3e2d84e5f0982c91862f344983d489094678
gorobchenkoann <gorobchenko.ann@gmail.com>
2018-12-31 13:48:20 +0300

    basic interface and functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import styles from './App.scss';
 
 export class App extends React.Component {
+    state = {
+        value: null
+    }
+
+    inputChangeHandler = e => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    btnClickHandler = e => {
+        e.preventDefault();
+        if (this.state.value) {
+            this.searchImage();
+        }
+    }
+
+    searchImage = () => {
+        console.log('Search image: ', this.state.value)
+    }
+
     render() {
         return (
-            <p>Edit App.js in 'src/components/App' folder to start</p>
+            <div className={styles.container}>
+                <form className={styles.form}>
+                    <input 
+                        type='text' 
+                        value={this.state.values}
+                        onChange={this.inputChangeHandler}
+                        className={styles.input} 
+                    />
+                    <button 
+                        type='submit' 
+                        onClick={this.btnClickHandler}
+                        className={styles.submit}
+                    >
+                        <FaSearch />
+                    </button>
+                </form>
+            </div>
         )
     }
 }
