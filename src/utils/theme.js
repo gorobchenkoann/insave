9b49b0ad84adf68ee7a091f9930aee3de22ce9c7
gorobchenkoann <gorobchenko.ann@gmail.com>
2019-01-02 22:20:20 +0300

    edit background colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -4,12 +4,12 @@ import bgLight from '../assets/bg-light.png';
 export const theme = {
     dark: {
         backgroundImage: `url(${bgDark})`,
-        backgroundColor: '#1f1f1f' ,
+        backgroundColor: '#0e121b',
         textColor: '#cacaca'
     },
     light: {
         backgroundImage: `url(${bgLight})`,
-        backgroundColor: '#cacaca' ,
+        backgroundColor: '#eceae8',
         textColor: '#1f1f1f'
     }
 }
