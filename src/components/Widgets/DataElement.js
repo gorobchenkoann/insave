724a685f3b2d50e5b9f48621bc4aa3942e9f3a68
gorobchenkoann <gorobchenko.ann@gmail.com>
2019-01-05 09:40:12 +0300

    add new folder

---
+++ src/components/Widgets/DataElement.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Image, Video, Slider } from '../';
+
+export const DataElement = ({dataType, data}) => {
+    switch (dataType) {
+        case 'image':
+            return (
+                <Image data={data} />
+            )
+        case 'video':
+            return (
+                <Video data={data} />
+            )
+        case 'slider':
+            return (
+                <Slider data={data} />
+            )
+        default:
+            return null
+    }
+}
